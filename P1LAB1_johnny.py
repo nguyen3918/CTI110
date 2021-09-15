d36b7c5d4fb537e91293588090dd08472aaa9947
nguyen3918 <90783408+nguyen3918@users.noreply.github.com>
2021-09-15 11:31:04 -0400

    First upload

---
+++ P1LAB1_johnny.py
@@ -0,0 +1,10 @@
+#CTI 110
+#P1LAB1 - zylab training
+#johnng nguyen
+#9/15/2021
+
+
+userNum = int(input())
+userNumSquared = userNum * userNum   # Bug here; fixed 
+   
+print(userNumSquared)       # Output formatting issue here; fix it when instructed
